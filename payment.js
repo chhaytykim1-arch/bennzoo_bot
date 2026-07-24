dab5f82c1244fb6b0f2c11764fbfbf8aca3e40d5
huotsoksithchey <huotsoksithchey@example.com>
2026-07-24 13:15:24 +0700

    Update Bakong merchant ID to kim_chhayhong@bkrt

--- payment.js
+++ payment.js
@@ -3,7 +3,7 @@ import QRCode from 'qrcode';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const DEFAULT_MERCHANT_ID = 'hut_soksitchey1@aclb';
+const DEFAULT_MERCHANT_ID = 'kim_chhayhong@bkrt';
 
 /**
  * Parse numeric USD amount from string or number
